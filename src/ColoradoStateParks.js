bc7f278b8fda2eab1140bb2f26eea6ee35276407
Jimmy <dm.a.uvarov@gmail.com>
2022-01-31 23:16:53 -0500

    commit tree 0d57b6e3312c0f68c6462632975789ecb2682ba9

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,23 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from ".parks/MesaVerdee"
+import {trees, wildlife} from "./parks/RockyMountain";
+
+console.log(trees);
+
+wildlife();
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  return (
+  <div>
+    <MesaVerde />
+  </div>
+  );
+  
+  // howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+
+  // return <h1>Colorado State Parks!</h1>;
 }
+
+export default ColoradoStateParks;
